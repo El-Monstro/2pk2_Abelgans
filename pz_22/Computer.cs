7418fd78ad40788b11a57bb7ad521f05629f008a
Кирилл Абельганс <kabelgans.2017@gmail.com>
2023-02-15 15:16:04 +0300

    2

---
+++ pz_22/Computer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_22
+{
+    internal class Computer
+    {
+        public int ID { get; set; }
+        public int totalHDDMemory { get; set; }
+        public int processorFrequency { get; set; }
+
+        public int AllHDD { get; set; }
+
+        private int frequency;
+        public int Frequency
+        {
+            get { return frequency; }
+            set
+            {
+                if (value <= 1.5 || value >= 5)
+
+                    Console.WriteLine("Ошибка. Частота должна быть в диапазоне от 1.5 до 5");
+                frequency = value;
+ 
+            }
+   
+        }
+        public Computer(int ID, int totalHDDMemory, int processorFrequency, int AllHDD)
+        {
+            this.ID = ID;
+            this.totalHDDMemory = totalHDDMemory;
+            this.processorFrequency = processorFrequency;
+            this.AllHDD = AllHDD;
+
+        }
+
+
+        public void GetCompName()
+        {
+            Console.WriteLine($"ID:{ID}\nПамять в ГБ:{totalHDDMemory}\nЧастота процессора:{processorFrequency}\nОбщая память всех компов: {AllHDD}");
+            
+        }
+
+    }
+}
---
+++ pz_22/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer L2301A = new Computer(231123, 222, 2, 123);
+            L2301A.GetCompName();
+        }
+    }
+}
