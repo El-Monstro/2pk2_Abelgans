2c1b9811b62b6f9f9b6919275ca29ae5ce25e6af
Кирилл Абельганс <kabelgans.2017@gmail.com>
2023-02-15 15:43:05 +0300

    need to finish( more

---
+++ pz_23/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Smartphone smartphone = new Smartphone();
+            smartphone.GetCompName();
+            // need to finish
+        }
+          
+    }
+}    
+    
+
---
+++ pz_23/Smartphone.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_23
+{
+    internal class Smartphone : Computer
+    {
+        
+    public double Diagonal { get; set; }
+        public string Diapazon { get; set; }
+        
+        public override string GetCompName()
+        {
+            return $"Diagonal: {Diagonal}, Diapazon: {Diapazon}";
+        }
+    }
+}
+
