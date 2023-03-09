bb172d38f22c6558819f3b373eb36b701a80bdab
Кирилл Абельганс <kabelgans.2017@gmail.com>
2023-03-09 18:53:54 +0300

    ban2

--- redaktor_txt_pz26/MainWindow.xaml.cs
+++ redaktor_txt_pz26/MainWindow.xaml.cs
@@ -24,5 +24,36 @@ namespace redaktor_txt_pz26
         {
             InitializeComponent();
         }
+
+        private void escButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void acceptButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Действие выполнено");
+        }
+
+        private void pressBold_Click(object sender, RoutedEventArgs e)
+        {
+            if (BAN.Selection.Text.Length > 0)
+            {
+                if (BAN.Selection.GetPropertyValue(TextElement.FontWeightProperty).Equals(FontWeights.Bold))
+                {
+                    BAN.Selection.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Normal);
+                }
+                else
+                {
+                    BAN.Selection.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Bold);
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+
+        }
     }
 }
