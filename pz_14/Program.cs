28155f0c8d74dfd943bfb1d88b17e0f3affd7c57
Кирилл Абельганс <kabelgans.2017@gmail.com>
2023-05-06 17:04:56 +0300

    .

---
+++ pz_14/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите номер элемента n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int a1 = 2;
+            int d = 6;
+
+            int nthElement = GetNthElement(a1, d, n);
+
+            Console.WriteLine($"n-й элемент арифметической прогрессии: {nthElement}");
+
+            Console.ReadKey();
+        }
+
+        static int GetNthElement(int a1, int d, int n)
+        {
+            if (n == 1)
+            {
+                return a1;
+            }
+
+            return GetNthElement(a1 + d, d, n - 1);
+        }
+    }
+    
+}
