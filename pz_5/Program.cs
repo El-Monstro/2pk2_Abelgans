4f8678f7ac172064cae95fd4da6fb055fe8a4e06
Кирилл Абельганс <kabelgans.2017@gmail.com>
2023-05-06 11:42:13 +0300

    1

---
+++ pz_5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int start = 0;
+            int end = 90;
+            int step = 5;
+
+            for (int i = start; i <= end; i += step)
+            {
+                Console.WriteLine(i);
+            }
+       
+
+            char startChar = 'b';
+            int n = 6;
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(startChar + " ");
+                startChar++;
+            }
+        }
+
+    }
+}
---
+++ pz_6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите начальный пробег спортсмена: ");
+            double x = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите желаемый пробег спортсмена: ");
+            double y = double.Parse(Console.ReadLine());
+
+            int day = 1;
+            while (x < y)
+            {
+                x *= 1.1;  
+                day++; 
+            }
+
+            Console.WriteLine("Пробег спортсмена достигнет значения {0} км на {1}-й день", y, day);
+        }
+    }
+}
