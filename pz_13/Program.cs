18611c6b81d00ee42b89360ab76c681cb947b156
Кирилл Абельганс <kabelgans.2017@gmail.com>
2023-05-06 17:02:46 +0300

    .

---
+++ pz_13/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            string input = Console.ReadLine();
+
+            double spacePercentage = GetSpacePercentage(input);
+
+            Console.WriteLine($"Процент пробелов в строке: {spacePercentage:F2}%");
+
+            Console.ReadKey();
+        }
+
+        static double GetSpacePercentage(string input)
+        {
+            int spaceCount = 0;
+
+            foreach (char c in input)
+            {
+                if (c == ' ')
+                {
+                    spaceCount++;
+                }
+            }
+
+            double spacePercentage = (double)spaceCount / input.Length * 100;
+
+            return spacePercentage;
+        }
+    }
+    
+}
