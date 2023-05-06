82d0780c608118776ad1c41dbec3780cd2b4d2f7
Кирилл Абельганс <kabelgans.2017@gmail.com>
2023-05-06 11:46:09 +0300

    1

---
+++ pz_7/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[50];
+            int[] filteredArray = new int[50];
+            Random rand = new Random();
+
+            // заполнение
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(-100, 101);
+            }
+
+            // копирование элементов в новый массив
+            int sum = 0;
+            int j = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] >= -10 && array[i] <= 10)
+                {
+                    filteredArray[j] = array[i];
+                    sum += Math.Abs(array[i]); 
+                    j++;
+                }
+            }
+
+            // вывод результатов
+            Console.WriteLine("Исходный массив:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine("\n\nОтфильтрованный массив:");
+            for (int i = 0; i < j; i++)
+            {
+                Console.Write(filteredArray[i] + " ");
+            }
+            Console.WriteLine("\n\nСумма по модулю: " + sum);
+        }
+    }
+}
---
+++ pz_8/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] arr = new char[8, 6];
+
+            
+            char c = 'A';
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    arr[i, j] = c;
+                    c++;
+                }
+            }
+
+            
+            Console.WriteLine("Исходный массив:");
+            PrintArray(arr);
+
+            
+            for (int j = 0; j < 6; j++)
+            {
+                char temp = arr[0, j];
+                arr[0, j] = arr[7, j];
+                arr[7, j] = temp;
+            }
+
+            
+            Console.WriteLine("Измененный массив:");
+            PrintArray(arr);
+        }
+
+
+        static void PrintArray(char[,] arr)
+        {
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write(arr[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
