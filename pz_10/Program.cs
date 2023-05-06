acabea81016682d5da566356d6a20637edcf9da5
Кирилл Абельганс <kabelgans.2017@gmail.com>
2023-05-06 16:54:26 +0300

    2

---
+++ pz_10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            string input = Console.ReadLine();
+
+            int upperCount = input.Count(char.IsUpper);
+            int lowerCount = input.Count(char.IsLower);
+            int spaceCount = input.Count(char.IsWhiteSpace);
+            int punctCount = input.Count(char.IsPunctuation);
+
+            Console.WriteLine($"Заглавных букв: {upperCount}");
+            Console.WriteLine($"Строчных букв: {lowerCount}");
+            Console.WriteLine($"Пробелов: {spaceCount}");
+            Console.WriteLine($"Символов пунктуации: {punctCount}");
+
+            Console.ReadKey();
+        }
+    }
+}
